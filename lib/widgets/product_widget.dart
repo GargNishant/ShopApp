720811c44ad7a103caa5523afaa333face1e4b8a
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-21 12:23:22 +0530

    *(product_widget.dart) OnTab added

--- lib/widgets/product_widget.dart
+++ lib/widgets/product_widget.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:shopapp/models/product.dart';
+import 'package:shopapp/routes/product_detail_route.dart';
 
 class ProductWidget extends StatelessWidget {
   final Product _product;
 
   ProductWidget(this._product);
 
+  void _navigation(BuildContext context){
+    Navigator.pushNamed(context, ProductDetailRoute.routeName,arguments: _product.id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(_product.imageUrl),
+      child: GestureDetector(
+        onTap: () => _navigation(context),
+        child: Image.network(_product.imageUrl),
+      ),
       footer: GridTileBar(
         backgroundColor: Colors.blueGrey.withOpacity(0.8),
         title: Text(
@@ -21,9 +29,9 @@ class ProductWidget extends StatelessWidget {
           onPressed: () {},
         ),
         trailing: IconButton(
-        icon: Icon(Icons.shopping_cart),
-        onPressed: () {},
-      ),
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
       ),
     );
   }
