96d7eb56d09ef4cbd4277675b49d6232f39ea229
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-21 17:51:53 +0530

    *(product_overview_route.dart) Updating only GridBuilder

--- lib/routes/product_overview_route.dart
+++ lib/routes/product_overview_route.dart
@@ -1,42 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:shopapp/repository/product.dart';
-import 'package:shopapp/widgets/product_grid_widget.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/repository/product_provider.dart';
+import 'package:shopapp/widgets/product_widget.dart';
 
 class ProductOverviewRoute extends StatelessWidget {
-  final List<Product> loadedProducts = [
-    Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +11,25 @@ class ProductOverviewRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("MyShop"),
       ),
-      body: ProductGridWidget(),
+      body: Consumer<ProductProvider>(
+        builder: (ctx, productProvider, child) => GridView.builder(
+          itemCount: productProvider.productList.length,
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              childAspectRatio: 16 / 9,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10),
+          itemBuilder: (ctx, index) {
+            /*We are returning Observer for each of product individually. With this the individual Widgets, will
+            not be rebuilding in change in some other item changes. Value binds the Notifier with it's values,
+            thus recycling it along with widget for List item* */
+            return ChangeNotifierProvider.value(
+              value: productProvider.productList[index],
+              child: ProductWidget(),
+            );
+          },
+        ),
+      )
     );
   }
 }
