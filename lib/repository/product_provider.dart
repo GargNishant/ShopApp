2352e1da7f727c1dde1bb295fb5968313f572a2a
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-21 16:23:05 +0530

    *(product_provider.dart) Initialize List

--- lib/repository/product_provider.dart
+++ lib/repository/product_provider.dart
@@ -1,21 +1,56 @@
-
 import 'package:flutter/material.dart';
-import 'package:shopapp/models/product.dart';
+import 'package:shopapp/repository/product.dart';
 
-
-class ProductProvider with ChangeNotifier{
+class ProductProvider with ChangeNotifier {
   List<Product> _productList = List();
   Map<String, Product> _idProductMap = Map();
 
+  ProductProvider() {
+    _productList.add(Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ));
+    _productList.add(Product(
+      id: 'p2',
+      title: 'Trousers',
+      description: 'A nice pair of trousers.',
+      price: 59.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    ));
+    _productList.add(Product(
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl:
+      'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ));
+    _productList.add(Product(
+      id: 'p4',
+      title: 'A Pan',
+      description: 'Prepare any meal you want.',
+      price: 49.99,
+      imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    ));
+    _idProductMap[_productList[0].id] = _productList[0];
+    _idProductMap[_productList[1].id] = _productList[1];
+    _idProductMap[_productList[2].id] = _productList[2];
+    _idProductMap[_productList[3].id] = _productList[3];
+  }
 
-  List<Product> get productList{
+  List<Product> get productList {
     return [..._productList];
   }
 
   /// Adding a new Item into the Existing List of products
   /// Calling the NotifyListeners is like calling setValue in Observers. But on all the member variables
-  void addProduct(Product product){
-
+  void addProduct(Product product) {
     notifyListeners();
   }
 
@@ -24,5 +59,4 @@ class ProductProvider with ChangeNotifier{
       return _idProductMap[id];
     return null;
   }
-
 }
