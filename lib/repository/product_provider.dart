31ce772942708c961c8cfde34aaa8a1c890df2d4
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-21 12:24:08 +0530

    +(product_provider.dart) Map for O(1) getting data

--- lib/repository/product_provider.dart
+++ lib/repository/product_provider.dart
@@ -5,6 +5,8 @@ import 'package:shopapp/models/product.dart';
 
 class ProductProvider with ChangeNotifier{
   List<Product> _productList = List();
+  Map<String, Product> _idProductMap = Map();
+
 
   List<Product> get productList{
     return [..._productList];
@@ -13,8 +15,14 @@ class ProductProvider with ChangeNotifier{
   /// Adding a new Item into the Existing List of products
   /// Calling the NotifyListeners is like calling setValue in Observers. But on all the member variables
   void addProduct(Product product){
-//    _productList.add(product);
+
     notifyListeners();
+  }
+
+  Product getProduct(String id){
+    if(_idProductMap[id] is Product)
+      return _idProductMap[id];
+    return null;
   }
 
 }
