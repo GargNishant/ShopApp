6869a80460b88be7b971ab35e199266cb711b772
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-22 18:35:56 +0530

    + cart.dart

---
+++ lib/repository/cart.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(
+      {@required this.id,
+      @required this.title,
+      @required this.quantity,
+      @required this.price});
+}
+
+class CartProvider with ChangeNotifier {
+  Map<String, CartItem> _cartItems;
+
+  Map<String, CartItem> get items {
+    return {..._cartItems};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_cartItems.containsKey(productId)) {
+      _cartItems.update(
+          productId,
+          (value) => CartItem(
+              id: value.id,
+              title: value.title,
+              price: value.price,
+              quantity: value.quantity + 1));
+    } else {
+      _cartItems.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              price: price,
+              quantity: 1));
+    }
+  }
+}
