ba3b477a6cf0f55d5285026d489435cb5433203e
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-24 02:08:47 +0530

    new Dependencies

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/nishant/Flutter SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/nishant/GitRepo/ShopApp/shop_app"
+export "FLUTTER_APPLICATION_PATH=/home/nishant/GitRepo/ShopApp"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
