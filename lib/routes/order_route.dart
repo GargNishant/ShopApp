816be21081bd644948b5616bc2885fff0c54f472
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-24 02:09:12 +0530

    + order_item_widget.dart, order_route.dart

---
+++ lib/routes/order_route.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/repository/order.dart';
+import 'package:shopapp/widgets/order_item_widget.dart';
+
+class OrdersRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _orderProvider = Provider.of<OrderProvider>(context);
+    final _orderList = _orderProvider.orders;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Orders"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return OrderItemWidget(_orderList[index]);
+        },
+        itemCount: _orderList.length,
+      ),
+    );
+  }
+}
