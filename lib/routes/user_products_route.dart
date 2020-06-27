0cc4fcb2d90d3db68d662af051ff867dec742032
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-27 11:10:28 +0530

    *(user_products_route.dart) Navigation to edit_product_route

--- lib/routes/user_products_route.dart
+++ lib/routes/user_products_route.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopapp/main_drawer.dart';
 import 'package:shopapp/repository/product_provider.dart';
+import 'package:shopapp/routes/edit_product_route.dart';
 import 'package:shopapp/widgets/user_product_widget.dart';
 
 class UserProductsRoute extends StatelessWidget {
@@ -20,7 +21,9 @@ class UserProductsRoute extends StatelessWidget {
             actions: <Widget>[
               IconButton(
                 icon: Icon(Icons.add),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context).pushNamed(EditProductRoute.routeName);
+                },
               ),
             ]),
         drawer: MainDrawer(),
