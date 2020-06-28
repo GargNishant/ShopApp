cdeb752269a3be0bd3787dc2227d813729dacf2e
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-28 11:41:11 +0530

    *(user_products_route.dart) Swipe Refresh function

--- lib/routes/user_products_route.dart
+++ lib/routes/user_products_route.dart
@@ -7,32 +7,36 @@ import 'package:shopapp/widgets/user_product_widget.dart';
 
 class UserProductsRoute extends StatelessWidget {
   static const routeName = "/user-product-route";
+
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<ProductProvider>(context).fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     final _productProvider = Provider.of<ProductProvider>(context);
     final _productList = _productProvider.productList;
     return Scaffold(
-        appBar: AppBar(
-            title: Text("Your Products"),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () {
-                  Navigator.of(context).pushNamed(EditProductRoute.routeName);
-                },
-              ),
-            ]),
+        appBar: AppBar(title: Text("Your Products"), actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => Navigator.of(context).pushNamed(EditProductRoute.routeName),
+          ),
+        ]),
         drawer: MainDrawer(),
-        body: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: ListView.builder(
-            itemCount: _productList.length,
-            itemBuilder: (ctx, index) {
-              return Column(children: <Widget>[
-                UserProductWidget(_productList[index]),
-                Divider(),
-              ]);
-            },
+        body: RefreshIndicator(
+          onRefresh: () => _refreshProducts(context),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: ListView.builder(
+              itemCount: _productList.length,
+              itemBuilder: (ctx, index) {
+                return Column(children: <Widget>[
+                  UserProductWidget(_productList[index]),
+                  Divider(),
+                ]);
+              },
+            ),
           ),
         ));
   }
