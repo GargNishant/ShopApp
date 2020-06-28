f47e44a7120d6d069a23910169cc2b55eff96564
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-28 21:36:26 +0530

    +(order.dart, order_route.dart) Fetch and Add Order async

--- lib/repository/order.dart
+++ lib/repository/order.dart
@@ -19,24 +19,55 @@ class Order with ChangeNotifier {
 
 class OrderProvider with ChangeNotifier {
   List<Order> _orders;
-  
+
   OrderProvider(){
     _orders = List();
   }
-  
+
   List<Order> get orders{
     return [..._orders];
   }
-  
-  Future<void> addOrder(List<CartItem> cartProducts, double totalAmount) async{
+
+  Future<void> fetchData() async {
+    final url = "https://flutter-shop-e0ed8.firebaseio.com/orders.json";
+    final response = await http.get(url);
+    final List<Order> items = List();
+    Map<String, dynamic> map = json.decode(response.body);
+
+    if(map == null){
+      _orders.clear();
+      notifyListeners();
+      return;
+    }
+
+    map.forEach((orderId, order) {
+      final orderItem = Order(
+        id: orderId,
+        amount: order['amount'],
+        dateTime: DateTime.parse(order['dateTime']),
+        products: (order['products'] as List<dynamic>).map((item) => CartItem.fromJsonMap(item)).toList()
+      );
+      items.add(orderItem);
+    });
+
+    _orders.clear();
+    _orders.addAll(items.reversed.toList());
+  }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double totalAmount) async {
     final url = "https://flutter-shop-e0ed8.firebaseio.com/orders.json";
     final timeStamp = DateTime.now();
-    final _response = await http.post(url,body: json.encode({
-      "dateTime": timeStamp.toIso8601String(),
-      "amount" : totalAmount,
-      "products": cartProducts.map((item) => item.toJsonMap()).toList(),
-    }));
-    _orders.add(Order(id: json.decode(_response.body)['name'],amount: totalAmount,dateTime: timeStamp,products: cartProducts));
+    final _response = await http.post(url,
+        body: json.encode({
+          "dateTime": timeStamp.toIso8601String(),
+          "amount": totalAmount,
+          "products": cartProducts.map((item) => item.toJsonMap()).toList(),
+        }));
+    _orders.add(Order(
+        id: json.decode(_response.body)['name'],
+        amount: totalAmount,
+        dateTime: timeStamp,
+        products: cartProducts));
     notifyListeners();
   }
 }
--- lib/routes/order_route.dart
+++ lib/routes/order_route.dart
@@ -4,23 +4,47 @@ import 'package:shopapp/main_drawer.dart';
 import 'package:shopapp/repository/order.dart';
 import 'package:shopapp/widgets/order_item_widget.dart';
 
-class OrdersRoute extends StatelessWidget {
+class OrdersRoute extends StatefulWidget {
   static const routeName = "/order-route";
+
+  @override
+  _OrdersRouteState createState() => _OrdersRouteState();
+}
+
+class _OrdersRouteState extends State<OrdersRoute> {
+  var _isInitialized = false;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    if (!_isInitialized) {
+      final orderProvider = Provider.of<OrderProvider>(context, listen: false);
+      setState(() => _isLoading = true);
+
+      orderProvider.fetchData().then((value) {
+        setState(() => _isLoading = false);
+      }).catchError((error) {
+        setState(() => _isLoading = false);
+      });
+    }
+    _isInitialized = true;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     final _orderProvider = Provider.of<OrderProvider>(context);
     final _orderList = _orderProvider.orders;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Your Orders"),
-      ),
-      body: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return OrderItemWidget(_orderList[index]);
-        },
-        itemCount: _orderList.length,
-      ),
+      appBar: AppBar(title: Text("Your Orders")),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return OrderItemWidget(_orderList[index]);
+              },
+              itemCount: _orderList.length,
+            ),
       drawer: MainDrawer(),
     );
   }
